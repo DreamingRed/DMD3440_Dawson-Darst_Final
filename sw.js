c12ae016946ac2bf59c04e7d8592feb1049bbba2
Adam Dawson-Darst <adawsondarst@outlook.com>
2023-12-06 12:34:12 -0500

    Manifest Changes.

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
-const cacheName = 'cacheMoney'
-const GHPATH = '/DMD3440_Dawson-Darst_Final'
+const cacheName = "cacheMoney"
+const GHPATH = "/DMD3440_Dawson-Darst_Final"
 
-const urlsToCache = [
+const URLS = [
     `${GHPATH}/`,
     `${GHPATH}/index.html`,
     `${GHPATH}/style.css`,
     `${GHPATH}/scripts/main.js`,
   ];
 
-self.addEventListener("install", event => {
-   event.waitUntil(
-      caches.open(cacheName)
-      .then(cache => {
-         return cache.addAll(urlsToCache)
+self.addEventListener("install", function (e) {
+   e.waitUntil(
+      caches.open(cacheName).then(function (cache) {
+         console.log(`Installing Cache: ${cache}\n`)
+         return cache.addAll(URLS)
       })
    )
 })
@@ -21,7 +21,6 @@ self.addEventListener("fetch", event => {
    event.respondWith(
      caches.match(event.request)
      .then(cachedResponse => {
-	   // It can update the cache to serve updated content on the next request
          console.log(`${cachedResponse} retrieved.`)
          return cachedResponse || fetch(event.request);
      }
